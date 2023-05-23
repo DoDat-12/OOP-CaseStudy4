88fa29a3b44d14876598251c29650ecae088c097
DoDat_12 <89787228+DoDat-12@users.noreply.github.com>
2023-05-23 09:25:52 +0700

    Missing addLoginListener method

--- CaseStudy04/com/dodat/client/ClientRun.java
+++
@@ -1,9 +0,0 @@
-package com.dodat.client;
-
-public class ClientRun {
-    public static void main(String[] args) {
-        RMILoginClientView clientView = new RMILoginClientView();
-        RMILoginClientControl clientControl = new RMILoginClientControl(clientView);
-        clientView.setVisible(true);
-    }
-}
--- CaseStudy04/com/dodat/server/ServerRun.java
+++
@@ -1,9 +0,0 @@
-package com.dodat.server;
-
-import java.rmi.RemoteException;
-public class ServerRun {
-    public static void main(String[] args) throws RemoteException {
-        RMILoginServerView serverView = new RMILoginServerView();
-        RMILoginServerControl serverControl = new RMILoginServerControl(serverView);
-    }
-}
